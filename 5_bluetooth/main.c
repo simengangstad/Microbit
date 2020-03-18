654bbbf556909a920c8cda3e98148ac0fc56f776
Simen Gangstad <simen.gangstad@me.com>
2020-03-19 00:02:45 +0100

    Add exercise 5

---
+++ 5_bluetooth/main.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "ubit.h"
+
+#include "bluetooth.h"
+
+int main(){
+	ubit_uart_init();
+
+	uint32_t err_code = 0;
+
+	err_code = bluetooth_init();
+	ubit_uart_print("BLE Enable error code: %d\n\r", err_code);
+	err_code = bluetooth_gap_advertise_start();
+
+	ubit_uart_print("BLE Advertise start error code: %d\n\r", err_code);
+
+	return 0;
+}
